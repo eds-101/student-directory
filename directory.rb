f55823ed3c41d9603b77a9ba54bc2c212779f18d
Eds <eds@edss-mbp-2.home>
2021-01-25 12:45:44 +0000

    Added feature to write students to CSV file

--- directory.rb
+++ directory.rb
@@ -26,7 +26,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
 end
 
 def process(selection)
@@ -35,6 +36,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -77,6 +80,18 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students.\n"
 end  
+
+def save_students
+  # open file for viewing
+  file = File.open("students.csv", "w") # use "a" to append instead of (over)write
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 interactive_menu
 
